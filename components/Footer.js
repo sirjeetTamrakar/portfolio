1b45f71f733acfe9af6386ff84f7e733431c3ea5
Srijeet-Tamrakar <tsirjeet220@gmail.com>
2021-06-18 13:52:26 +0545

    fourth

--- components/Footer.js
+++ components/Footer.js
@@ -5,16 +5,15 @@ const Footer = () => {
     return (
         <>
             <div id='contact' className={`${styles.footer} px-4`}>
-            <h4>Copyright &copy; 2021 Sirjeet Tamrakar | All Rights Reserved.</h4>
                 <div className={styles.icon}>
                     <a href="https://www.instagram.com/__srijeet__/" target='blank' className={styles.a}><i className="fab fa-instagram"></i></a>
                     <a href="https://www.facebook.com/sirjeet.tamrakar" target='blank' className={styles.a}><i className="fab fa-facebook"></i></a>
-                    <a href="https://www.snapchat.com/add/s_tamrakara" target='blank' className={styles.a}><i className="fab fa-snapchat-ghost"></i></a>
+                    <a href="https://www.linkedin.com/in/sirjeet-tamrakar-473b11214/" target='blank' className={styles.a}><i className="fab fa-linkedin"></i></a>
                 </div>
                 <div className={styles.phone}>
-                    <h6 className={styles.i}><i className="fas fa-phone-alt"></i> 9860913929</h6>
-                    <h6 className={styles.i}><i className="fas fa-home"></i> Baluwatar, Kathmandu</h6>
-                    <h6 className={styles.i}><i className="fas fa-envelope"></i> sirjeettamrakar007@gmail.com</h6>
+                    <h6 className={styles.i}><i className="fas fa-phone-alt"></i>&nbsp;&nbsp;9860913929</h6>
+                    <h6 className={styles.i}><i className="fas fa-home"></i>&nbsp;&nbsp; Baluwatar, Kathmandu</h6>
+                    <h6 className={styles.i}><i className="fas fa-envelope"></i>&nbsp;&nbsp; sirjeettamrakar007@gmail.com</h6>
                 </div>
                     <div className='mt-4'>Made With
                     </div>
