cddc5791b5ce1f8683a6480eb9eb1b439bc9a34f
Srijeet-Tamrakar <tsirjeet220@gmail.com>
2021-07-14 17:35:18 +0545

    ok

--- components/Hero.js
+++ components/Hero.js
@@ -5,16 +5,50 @@ import styles from '../styles/Hero.module.css'
 
 const Hero = () => {
     return (
-        <div id='home'  className={styles.hero}>
-            <div className={styles.me} >
-                <h1 data-aos="zoom-in-up" className={styles.title}>Hi, I am Srijeet.<br />I am a Full Stack Developer</h1>
-                <Link   href='#contact'><button  id='projects' data-aos="zoom-in-right" data-aos-offset="100" className={styles.button}>Contact Me</button></Link>
-            </div>
-            <div  className={styles.overlay} data-aos-offset="100" data-aos-duration="3000" data-aos="zoom-in-left">
-                <Image src='/images/i.png' alt='Srijeet Tamrakar' priority width={1080} height={810}  className={styles.image} />
-            </div>
-        </div>
-    )
+			<div id='home' className={styles.hero}>
+				<div className={styles.me}>
+					<h1 data-aos='zoom-in-up' data-aos-delay='1000' data-aos-once='true'>
+						Hi, I am Srijeet.
+					</h1>
+					<h1
+						data-aos='fade-up'
+						data-aos-delay='1500'
+						data-aos-once='true'
+						className={styles.title}
+					>
+						I am a Full Stack Developer
+					</h1>
+					<br />
+					<Link href='#contact'>
+						<button
+							id='projects'
+							data-aos='zoom-in-right'
+							data-aos-delay='1800'
+							data-aos-once='true'
+							className={styles.button}
+						>
+							Contact Me
+						</button>
+					</Link>
+				</div>
+				<div
+					className={styles.overlay}
+					data-aos-duration='2000'
+					data-aos='zoom-in-left'
+					data-aos-once='true'
+				>
+					<Image
+						src='/images/1.png'
+						alt='Srijeet Tamrakar'
+						priority
+						width={1080}
+						height={810}
+						objectFit='contain'
+						className={styles.image}
+					/>
+				</div>
+			</div>
+		);
 }
 
 export default Hero
--- components/NavBar.js
+++ components/NavBar.js
@@ -6,20 +6,69 @@ import styles from '../styles/Navbar.module.css'
 const NavBar = () => {
     const [expanded, setExpanded] = useState(false)
     return (
-        <>
-            <Navbar expanded={expanded} className={styles.header} expand="md">
-                <Navbar.Toggle style={{color:'white'}} onClick={() => setExpanded(expanded ? false : "expanded")} >{!expanded ? <i className="fas fa-bars"></i> : <i className="fas fa-times"></i>}</Navbar.Toggle>
-                <Navbar.Collapse onClick={() => setExpanded(expanded ? false : "expanded")} className={`justify-content-start ${styles.collapse}`}>
-                <Nav>
-                    <Link href='#home'><a className={styles.linki} > HOME </a></Link>
-                    <Link href='#projects'><a className={styles.linki} >PROJECTS</a></Link>
-                    <Link href='#about'><a className={styles.linki} >ABOUT</a></Link>     
-                    <Link href='#contact'><a className={styles.linki} >CONTACT</a></Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            </>
-    )
+			<>
+				<Navbar expanded={expanded} className={styles.header} expand='md'>
+					<Navbar.Toggle
+						style={{color: "white"}}
+						onClick={() => setExpanded(expanded ? false : "expanded")}
+					>
+						{!expanded ? (
+							<i className='fas fa-bars'></i>
+						) : (
+							<i className='fas fa-times'></i>
+						)}
+					</Navbar.Toggle>
+					<Navbar.Collapse
+						onClick={() => setExpanded(expanded ? false : "expanded")}
+						className={`justify-content-start ${styles.collapse}`}
+					>
+						<Nav>
+							<Link href='#home'>
+								<a
+									data-aos-delay='2500'
+									data-aos='zoom-in-up'
+									data-aos-once='true'
+									className={styles.linki}
+								>
+									{" "}
+									HOME{" "}
+								</a>
+							</Link>
+							<Link href='#projects'>
+								<a
+									data-aos-delay='2600'
+									data-aos='zoom-in-up'
+									data-aos-once='true'
+									className={styles.linki}
+								>
+									PROJECTS
+								</a>
+							</Link>
+							<Link href='#about'>
+								<a
+									data-aos-delay='2700'
+									data-aos='zoom-in-up'
+									data-aos-once='true'
+									className={styles.linki}
+								>
+									ABOUT
+								</a>
+							</Link>
+							<Link href='#contact'>
+								<a
+									data-aos-delay='2800'
+									data-aos='zoom-in-up'
+									data-aos-once='true'
+									className={styles.linki}
+								>
+									CONTACT
+								</a>
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</>
+		);
 }
 
 export default NavBar
