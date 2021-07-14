10a50c274fad00d9ab0fbe9fc26760dd3972fe19
Srijeet-Tamrakar <tsirjeet220@gmail.com>
2021-07-14 10:05:10 +0545

    ok

--- components/Projects.js
+++ components/Projects.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import { Carousel } from 'react-bootstrap'
-import { projects } from '../constants/constants'
 import styles from '../styles/Projects.module.css'
 
-const Projects = () => {
+const Projects = ({ projectList }) =>
+{
     return (
         <div >
         <h1  className={styles.head}>PROJECTS</h1>
         <div className={styles.main} >
-            {projects.map(({title, visit, images, tags, id }) => (
+            {projectList.map(({title, visit, images, tags, id }) => (
                 <div className={styles.div} data-aos="zoom-in-up" key={id} >
                     <div className={styles.overflow} >
                         <Carousel  nextIcon='' prevIcon=''>
                             {images.map((image, i) => (
                                 <Carousel.Item key={i}>
-                                    <Image data-aos="zoom-in-down" placeholder='blur'  priority className={styles.image} src={image} height={810} width={1080} quality={10} alt='project_image' />
+                                    <Image data-aos="zoom-in-down" className={styles.image} src={image} height={810} width={1080} alt='project_image' />
                                 </Carousel.Item>
                             ))}
                         </Carousel>
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,22 @@ import Projects from '../components/Projects'
 import Technologies from '../components/Technologies'
 import Timeline from '../components/Timeline'
 import About from '../components/About'
+import {projects} from '../constants/constants'
 
-const Home = () => {
+export const getStaticProps = async () =>
+{
+    return {
+        props: {
+            projectList:projects
+        }
+    }
+}
+
+const Home = ({ projectList }) => {
   return (
     <>
       <Hero />
-      <Projects/>
+      <Projects projectList={projectList}/>
       <Technologies/>
       <Timeline />
       <About/>
